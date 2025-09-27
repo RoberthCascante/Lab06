fcb239e01e6bf74d966663e3721027d11e1a128a
RoberthCascante <robert.cascante.lopez@est.una.ac.cr>
2025-09-27 10:23:07 -0600

    Actualizar la URL de redirección en la configuración de OIDC a la dirección de producción

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ let oidc = new ExpressOIDC({
   client_id: OKTA_CLIENT_ID,
   client_secret: OKTA_CLIENT_SECRET,
   redirect_uri: REDIRECT_URI,
-  routes: { callback: { defaultRedirect: "http://localhost:3000/dashboard" } },
+  routes: { callback: { defaultRedirect: "https://lab06-iota.vercel.app/dashboard" } },
   scope: 'openid profile'
 });
 
