3889b2aac9fd281661f7e410d66ca3ad053516c3
RoberthCascante <robert.cascante.lopez@est.una.ac.cr>
2025-10-04 11:30:33 -0600

    fix: comentar línea de configuración de timeout en openIdClient

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/dashboard", requiresAuth() ,(req, res) => {
 });
 
 const openIdClient = require('openid-client');
-openIdClient.Issuer.defaultHttpOptions.timeout = 20000;
+//openIdClient.Issuer.defaultHttpOptions.timeout = 20000;
 
 oidc.on("ready", () => {
   console.log("Server running on port: " + PORT);
